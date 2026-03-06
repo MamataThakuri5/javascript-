f52979393637f03a14143239337d90cf82d0f6f3
MamataThakuri5 <mamatamallathakuri47@gmail.com>
2026-03-06 18:16:49 +0000

    conversion of data types

---
+++ basics_02/conversion.js
@@ -0,0 +1,21 @@
+let score="33abc";
+console.log(score);
+console.log(typeof(score));
+let valueInNumber=Number(score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+//undeffined=>NaN
+//null=>0
+let isLogedIn=1; // 1 is true and " " is false
+ 
+let booleanIsLogedIn=Boolean(isLogedIn);
+console.log(booleanIsLogedIn);
+isLogedIn=0;
+let num=String(30)
+console.log(num);
+console.log(typeof num);
