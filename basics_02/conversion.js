0ffe856e0dd9f3acd56fc191750d461466480f3f
MamataThakuri5 <mamatamallathakuri47@gmail.com>
2026-03-07 17:58:38 +0000

    added

--- basics_02/conversion.js
+++ basics_02/conversion.js
@@ -13,9 +13,36 @@ console.log(valueInNumber);
 //null=>0
 let isLogedIn=1; // 1 is true and " " is false
  
-let booleanIsLogedIn=Boolean(isLogedIn);
-console.log(booleanIsLogedIn);
-isLogedIn=0;
-let num=String(30)
-console.log(num);
-console.log(typeof num);
+// let booleanIsLogedIn=Boolean(isLogedIn);
+// console.log(booleanIsLogedIn);
+// isLogedIn=0;
+// let num=String(30)
+// console.log(num);
+// console.log(typeof num);
+
+
+/* operations*/
+let value=3
+let negvalue=-value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1="hello";
+let str2="world";
+console.log(str1+str2);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);//122
+console.log("1"+2+"2");//122
+console.log(1+2+"2");//32
+console.log(true);
+console.log(true+true);
+console.log(+true);
+
