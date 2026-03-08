2c2edbff2ca0d0a59c187ebeb541012a81a20d5f
MamataThakuri5 <mamatamallathakuri47@gmail.com>
2026-03-08 18:13:45 +0000

    stackHeap

---
+++ basics_02/stackHeapmemory.js
@@ -0,0 +1,33 @@
+// stack(premitive satatypes) 
+// heap(non-primitive data types)
+let myname="mamata";
+let myname2=myname;
+ myname2="thakuri";
+ console.log(myname);
+console.log(myname2);
+let  surname="thakuri";
+console.log(surname);
+let student={
+  name:"radha:",
+  age:20,
+
+}
+let student2=student;
+student2.name="sita";
+student.age=30;
+console.log(student);
+console.log(student2);
+
+
+const name="Krishna";
+const repo=4;
+console.log(`hello my name is ${name} and i have ${repo} repo`);
+
+const username= new String("mamata");
+console.log(username[0]);
+// built in functions
+console.log(username.length);
+console.log(username.toUpperCase());
+console.log(username.charAt(3));
+console.log(username.indexOf("a"));
+
