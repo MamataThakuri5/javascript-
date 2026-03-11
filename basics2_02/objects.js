53f5914818a7dd08f8ae042841d34b4b146b5441
MamataThakuri5 <mamatamallathakuri47@gmail.com>
2026-03-11 18:28:37 +0000

    objects

---
+++ basics2_02/objects.js
@@ -0,0 +1,12 @@
+//singleton object created
+//object literals
+
+const jsUser={
+  name:"mamata",
+  age:20,
+  location:"rvs",
+  isLoggedIn:true,
+  lastLoginDays:["monday","saturay"]
+}
+console.log(jsUser.location)
+console.log(jsUser["location"])
