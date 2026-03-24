37276a00f02a2f278a5d8023e6bc1838659d7154
MamataThakuri5 <mamatamallathakuri47@gmail.com>
2026-03-24 18:24:28 +0000

    projects

--- project3/script.js
+++ project3/script.js
@@ -1,7 +1,14 @@
-const clock=document.getElementById("clock");
-// const clock=document.querySelector('#clock');
-(date.toLocaleTimeString());
-setInterval(function(){
-  let date=new Date();
-//console.log(date.toLocaleDateString());
-clock.innerHTML=date.toLocaleDateString();},1000)
+//const clock=document.getElementById('clock');
+// // const clock=document.querySelector('#clock');
+// //(date.toLocaleTimeString());
+// setInterval(function(){
+//   let date=new Date();
+// //console.log(date.toLocaleDateString());
+// clock.innerHTML=date.toLocaleTimeString();
+// },1000);
+const clock = document.getElementById('clock');
+
+setInterval(function () {
+  const date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
